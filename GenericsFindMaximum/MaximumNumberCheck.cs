363ff1ed5531c52e4d68dadba745b876dc208b96
NikitaSalunke10 <salunkenikita10@gmail.com>
2022-07-28 17:13:20 +0530

    using sort finding the maximum

--- GenericsFindMaximum/MaximumNumberCheck.cs
+++ GenericsFindMaximum/MaximumNumberCheck.cs
@@ -67,5 +67,36 @@ namespace GenericsFindMaximum
             }
             throw new Exception("firstNumber,secondNumber and thirdNumber are same");
         }
+        //this method is used to find maximum integer number in array
+        public static int findMax(int[] arr)
+        {
+            Array.Sort(arr); // the sort method from Array class is used to sort the array in ascending order
+            Console.WriteLine("The sorted Integer array is :");
+            for (int i = 0; i < arr.Length; i++) //this for loop is used to print values one by one from array
+            {
+                Console.WriteLine(arr[i]);
+            }
+            return arr[arr.Length-1]; // returning the last value in array i.e the maximum number
+        }
+        public static double findMax(double[] arr)
+        {
+            Array.Sort(arr); // the sort method from Array class is used to sort the array in ascending order
+            Console.WriteLine("The sorted Double array is :");
+            for (int i = 0; i < arr.Length; i++) //this for loop is used to print values one by one from array
+            {
+                Console.WriteLine(arr[i]);
+            }
+            return arr[arr.Length - 1]; // returning the last value in array i.e the maximum double number
+        }
+        public static string findStringMax(string[] arr)
+        {
+            Array.Sort(arr); // the sort method from Array class is used to sort the array in ascending order
+            Console.WriteLine("The sorted String array is :");
+            for (int i = 0; i < arr.Length; i++) //this for loop is used to print values one by one from array
+            {
+                Console.WriteLine(arr[i]);
+            }
+            return arr[arr.Length - 1]; // returning the last value in array i.e the maximum string
+        }
     }
 }
--- GenericsFindMaximum/Program.cs
+++ GenericsFindMaximum/Program.cs
@@ -6,5 +6,18 @@
 //double doubleoutput = MaximumNumberCheck.MaximumFloatNumber(52.1, 76.92, 25.65);
 //Console.WriteLine("Maximum Float Number is "+doubleoutput);
 
-string stringOutput = MaximumNumberCheck.MaximumStringNumber("Apple", "Peach", "Banana");
-Console.WriteLine("Maximum String input is " + stringOutput);
+//string stringOutput = MaximumNumberCheck.MaximumStringNumber("Apple", "Peach", "Banana");
+//Console.WriteLine("Maximum String input is " + stringOutput);
+
+int[] arr = { 24, 65, 23, 54, 76, 45, 19 };
+int output = MaximumNumberCheck.findMax(arr);
+Console.WriteLine("Maximum number is "+output);
+
+double[] doublearr = { 52.1, 76.92, 25.65, 63.76 };
+double doubleoutput = MaximumNumberCheck.findMax(doublearr);
+Console.WriteLine("Maximum number is " + doubleoutput);
+
+
+string[] stringarr = { "Apple", "Peach", "Banana", "Orange" };
+string stringoutput = MaximumNumberCheck.findStringMax(stringarr);
+Console.WriteLine("Maximum number is " + stringoutput);
