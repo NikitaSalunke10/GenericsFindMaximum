81da745a4447085a1ce1a24381fdc18a22281cbe
NikitaSalunke10 <salunkenikita10@gmail.com>
2022-07-28 16:33:41 +0530

    Find maximum from 3 string numbers

--- GenericsFindMaximum/MaximumNumberCheck.cs
+++ GenericsFindMaximum/MaximumNumberCheck.cs
@@ -42,5 +42,30 @@ namespace GenericsFindMaximum
             }
             throw new Exception("firstNumber,secondNumber and thirdNumber are same");
         }
+        //this method is used to find which String input is maximum
+        public static string MaximumStringNumber(string firstString, string secondString, string thirdString)
+        {
+            if (firstString.CompareTo(secondString) > 0 && firstString.CompareTo(thirdString) > 0 ||
+                firstString.CompareTo(secondString) >= 0 && firstString.CompareTo(thirdString) > 0 ||
+                firstString.CompareTo(secondString) > 0 && firstString.CompareTo(thirdString) >= 0)
+            {
+                return firstString;
+            }
+
+            if (secondString.CompareTo(firstString) > 0 && secondString.CompareTo(thirdString) > 0 ||
+                secondString.CompareTo(firstString) >= 0 && secondString.CompareTo(thirdString) > 0 ||
+                secondString.CompareTo(firstString) > 0 && secondString.CompareTo(thirdString) >= 0)
+            {
+                return secondString;
+            }
+
+            if (thirdString.CompareTo(firstString) > 0 && thirdString.CompareTo(secondString) > 0 ||
+                thirdString.CompareTo(firstString) >= 0 && thirdString.CompareTo(secondString) > 0 ||
+                thirdString.CompareTo(firstString) > 0 && thirdString.CompareTo(secondString) >= 0)
+            {
+                return thirdString;
+            }
+            throw new Exception("firstNumber,secondNumber and thirdNumber are same");
+        }
     }
 }
--- GenericsFindMaximum/Program.cs
+++ GenericsFindMaximum/Program.cs
@@ -3,5 +3,8 @@
 //int output = MaximumNumberCheck.MaximumIntegerNumber(23, 18, 10);
 //Console.WriteLine("Maximum Number is "+output);
 
-double doubleoutput = MaximumNumberCheck.MaximumFloatNumber(52.1, 76.92, 25.65);
-Console.WriteLine("Maximum Float Number is "+doubleoutput);
+//double doubleoutput = MaximumNumberCheck.MaximumFloatNumber(52.1, 76.92, 25.65);
+//Console.WriteLine("Maximum Float Number is "+doubleoutput);
+
+string stringOutput = MaximumNumberCheck.MaximumStringNumber("43", "12", "76");
+Console.WriteLine("Maximum String Number is " + stringOutput);
