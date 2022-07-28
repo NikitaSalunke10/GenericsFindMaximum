d47d02d8893237dcff96be3150b108d4c6294f36
NikitaSalunke10 <salunkenikita10@gmail.com>
2022-07-28 16:36:49 +0530

    Find maximum from 3 strings

--- GenericsFindMaximum/Program.cs
+++ GenericsFindMaximum/Program.cs
@@ -6,5 +6,5 @@
 //double doubleoutput = MaximumNumberCheck.MaximumFloatNumber(52.1, 76.92, 25.65);
 //Console.WriteLine("Maximum Float Number is "+doubleoutput);
 
-string stringOutput = MaximumNumberCheck.MaximumStringNumber("43", "12", "76");
-Console.WriteLine("Maximum String Number is " + stringOutput);
+string stringOutput = MaximumNumberCheck.MaximumStringNumber("Apple", "Peach", "Banana");
+Console.WriteLine("Maximum String input is " + stringOutput);
