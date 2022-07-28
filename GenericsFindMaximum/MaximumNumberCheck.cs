efe8fd30eede444ea8c19ac3e7a6b06869459a25
NikitaSalunke10 <salunkenikita10@gmail.com>
2022-07-28 16:24:14 +0530

    Find maximum from 3 float numbers

--- GenericsFindMaximum/MaximumNumberCheck.cs
+++ GenericsFindMaximum/MaximumNumberCheck.cs
@@ -8,8 +8,25 @@ namespace GenericsFindMaximum
 {
     internal class MaximumNumberCheck
     {
-        //This method is used to find which number is maximum
+        //This method is used to find which integer number is maximum
         public static int MaximumIntegerNumber(int firstValue, int secondValue, int thirdValue)
+        {
+            if (firstValue.CompareTo(secondValue) > 0 && firstValue.CompareTo(thirdValue) > 0)
+            {
+                return firstValue;
+            }
+            if (secondValue.CompareTo(firstValue) > 0 && secondValue.CompareTo(thirdValue) > 0)
+            {
+                return secondValue;
+            }
+            if (thirdValue.CompareTo(firstValue) > 0 && thirdValue.CompareTo(secondValue) > 0)
+            {
+                return thirdValue;
+            }
+            throw new Exception("firstNumber,secondNumber and thirdNumber are same");
+        }
+        //this method is used to find which float number is maximum
+        public static double MaximumFloatNumber(double firstValue, double secondValue, double thirdValue)
         {
             if (firstValue.CompareTo(secondValue) > 0 && firstValue.CompareTo(thirdValue) > 0)
             {
--- GenericsFindMaximum/Program.cs
+++ GenericsFindMaximum/Program.cs
@@ -1,4 +1,7 @@
 ﻿using GenericsFindMaximum;
 
-int output = MaximumNumberCheck.MaximumIntegerNumber(23, 18, 10);
-Console.WriteLine("Maximum Number is "+output);
+//int output = MaximumNumberCheck.MaximumIntegerNumber(23, 18, 10);
+//Console.WriteLine("Maximum Number is "+output);
+
+double doubleoutput = MaximumNumberCheck.MaximumFloatNumber(52.1, 76.92, 25.65);
+Console.WriteLine("Maximum Float Number is "+doubleoutput);
